63fbeb5aca784b19771c066cbe615770d98d7c24
Lenovo <Lenovo@DESKTOP-11CB7JG>
2022-08-05 17:31:15 +0300

    Постарался все учесть

--- Prostoe chislo/Program.cs
+++ Prostoe chislo/Program.cs
@@ -8,32 +8,68 @@ namespace Prostoe_chislo
 {
     internal class Program
     {
-        static int SumOfElements(int[]myArray)
+       
+        static void SimpleNumber(double divider, double sumOfElement)
         {
-            int SumOfElements = 0;
+            bool simpleNumber = ((sumOfElement / divider != divider)&& (sumOfElement % 2 != 0)) ? true : false;
+            Console.WriteLine("Простое число: " + simpleNumber);
+        }
+        static void SimpleNumberException(double divider, double sumOfElement)
+        {
+            if ((sumOfElement / divider != divider) && (sumOfElement % 2 != 0))
+            {
+                Console.WriteLine("Простое число: " + true);
+            }
+            else
+            {
+                Console.WriteLine("Простое число: " + false);
+            }
+        }
+
+        static void FindOfDivider(double sumOfElement)
+        {
+            double divider = 0;
+            for (double i = 2; i < sumOfElement; i++)
+            {
+                if (i == (sumOfElement / i))
+                {
+                     divider= i;
+                    Console.WriteLine(i);
+                    SimpleNumber(divider, sumOfElement);
+                    break;
+                }
+            }
+                SimpleNumberException(divider, sumOfElement);
+
+        }
+        static double SumOfElements(int[] myArray)
+        {
+            double sumOfElements = 0;
             for (int i = 0; i < myArray.Length; i++)
             {
-                SumOfElements += myArray[i];
+                sumOfElements += myArray[i];
             }
-            if (SumOfElements%2 !=0)
-            {
-                return SumOfElements;
-            }
-            return 0;
+            return sumOfElements;
         }
-      
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Vvedite massiv");
-            int myMassiv=int.Parse(Console.ReadLine());
-            int[] myArray = new int[myMassiv];
+            Console.Write("Введите количество элементов массива: ");
+            double myMassiv = double.Parse(Console.ReadLine());
+            int[] myArray = new int[(int)myMassiv];
             for (int i = 0; i < myArray.Length; i++)
             {
-                Console.Write($"Vvedite element pod indexom {i}: ");
+                Console.Write($"Введите элемент под индексом {i}: ");
                 myArray[i] = int.Parse(Console.ReadLine());
             }
-            int result = SumOfElements(myArray);
+            double result = SumOfElements(myArray);
+           FindOfDivider(result);
             Console.WriteLine(result);
+       
+          
+
+
+           
         }
     }
 }
