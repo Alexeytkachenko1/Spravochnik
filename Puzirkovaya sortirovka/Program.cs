39b53cb96610d3bd0b33fb6d9e0fd6f84aff896e
Lenovo <Lenovo@DESKTOP-11CB7JG>
2022-08-08 09:43:49 +0300

    Пузырьковая сортировка

---
+++ Puzirkovaya sortirovka/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzirkovaya_sortirovka
+{
+    class Program
+    {
+        static void Swap(ref int firstValue,ref int secondValue)
+        {
+            (secondValue, firstValue) = (firstValue, secondValue);
+        }
+        static int[] BubleSort(ref int[] arrayForSort)
+        {
+            for (int i = 0; i < arrayForSort.Length; i++)
+            {
+
+                for (int j = 0; j < arrayForSort.Length-1; j++)
+                {
+                    if (arrayForSort[j] > arrayForSort[j+1])
+                    {
+                        Swap(ref arrayForSort[j],ref arrayForSort[j+1]);
+                    }
+                }
+            }
+            return arrayForSort;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sortirovka puzurkom");
+            Console.WriteLine("Vvedite kolvo elementov masssiva");
+            int amountOfElements = int.Parse(Console.ReadLine());
+            
+            int[] arrayForSort = new int[amountOfElements];
+            for (int i = 0; i <arrayForSort.Length; i++)
+            {
+                Console.WriteLine($"Vvedite elemet pod nomerom {i}");
+                arrayForSort[i] = int.Parse(Console.ReadLine());
+            }
+            int []newArray= BubleSort(ref arrayForSort);
+            foreach (var item in newArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Puzirkovaya sortirovka/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Puzirkovaya sortirovka")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Puzirkovaya sortirovka")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("899a14c5-23c0-421c-a547-283beb6e0120")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
